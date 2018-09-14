2100ee685379bf9fc635d3096bcc08278b6e00a2
Borui Zhang <breezhang8160@gmail.com>
2018-09-14 14:57:25 -0500

    Added Newari consonant words, updated SRG reading materials

--- js/newariSound.js
+++ js/newariSound.js
@@ -55,5 +55,155 @@ var audio = new Audio('../newarisounds/jha-heavy.wav');
 audio.play()
 });
 
+// ta
+$('#ta').click(function(){
+console.log("ta");
+var audio = new Audio('../newarisounds/sister.wav');
+audio.play()
+});
+
+// tha
+$('#tha').click(function(){
+console.log("ta");
+var audio = new Audio('../newarisounds/self.wav');
+audio.play()
+});
+
+// da
+$('#da').click(function(){
+console.log("da");
+var audio = new Audio('../newarisounds/stage.wav');
+audio.play()
+});
+
+// dha
+$('#dha').click(function(){
+console.log("dha");
+var audio = new Audio('../newarisounds/metal.wav');
+audio.play()
+});
+
+// na
+$('#na').click(function(){
+console.log("na");
+var audio = new Audio('../newarisounds/Newa.wav');
+audio.play()
+});
+
+// pa
+$('#pa').click(function(){
+console.log("pa");
+var audio = new Audio('../newarisounds/spicy.wav');
+audio.play()
+});
+
+// pha
+$('#pha').click(function(){
+console.log("pha");
+var audio = new Audio('../newarisounds/wind.wav');
+audio.play()
+});
+
+// ba
+$('#ba').click(function(){
+console.log("ba");
+var audio = new Audio('../newarisounds/red_carpet.wav');
+audio.play()
+});
+
+// bha
+$('#bha').click(function(){
+console.log("bha");
+var audio = new Audio('../newarisounds/little.wav');
+audio.play()
+});
+
+// ma
+$('#ma').click(function(){
+console.log("ma");
+var audio = new Audio('../newarisounds/child.wav');
+audio.play()
+});
+
+// ma
+$('#ma').click(function(){
+console.log("ma");
+var audio = new Audio('../newarisounds/child.wav');
+audio.play()
+});
+
+// ya
+$('#ya').click(function(){
+console.log("ya");
+var audio = new Audio('../newarisounds/quickly.wav');
+audio.play()
+});
+
+// ra
+$('#ra').click(function(){
+console.log("ra");
+var audio = new Audio('../newarisounds/gem.wav');
+audio.play()
+});
+
+// la
+$('#la').click(function(){
+console.log("la");
+var audio = new Audio('../newarisounds/shoes.wav');
+audio.play()
+});
+
+// wa
+$('#wa').click(function(){
+console.log("wa");
+var audio = new Audio('../newarisounds/respected_3rd.wav');
+audio.play()
+});
+
+// sha
+$('#sha').click(function(){
+console.log("sha");
+var audio = new Audio('../newarisounds/peace.wav');
+audio.play()
+});
+
+
+// sa
+$('#sa').click(function(){
+console.log("sa");
+var audio = new Audio('../newarisounds/hair_band.wav');
+audio.play()
+});
+
+// ha
+$('#ha').click(function(){
+console.log("ha");
+var audio = new Audio('../newarisounds/again.wav');
+audio.play()
+});
+
+
+// chhya
+$('#chhya').click(function(){
+console.log("chhya");
+var audio = new Audio('../newarisounds/forgiveness.wav');
+audio.play()
+});
+
+// tra
+$('#tra').click(function(){
+console.log("tra");
+var audio = new Audio('../newarisounds/weapon.wav');
+audio.play()
+});
+
+// gya
+$('#gya').click(function(){
+console.log("gya");
+var audio = new Audio('../newarisounds/knowledge.wav');
+audio.play()
+});
+
+
 
 });
